c4b79f587e5d671133a734818506405ff2dbd275
Mohd Arif <31352655+mohdarif8299@users.noreply.github.com>
2024-01-28 20:33:31 +0000

    Create KthSmallestInMatrix.java

---
+++ KthSmallestInMatrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int kthSmallest(int[][] A, int B) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<A.length;i++) {
+            for(int j=0;j<A[i].length;j++) {
+                if(maxHeap.size()<B)
+                   maxHeap.add(A[i][j]); 
+                else { 
+                   if(A[i][j]<maxHeap.peek()) {
+                       maxHeap.poll();
+                       maxHeap.add(A[i][j]);
+                   } 
+                }
+            }
+        }
+        return maxHeap.poll();
+    }
+}
