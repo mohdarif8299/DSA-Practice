c1a7e60804f5e7e4705993ef6b097ec119302227
Mohd Arif <31352655+mohdarif8299@users.noreply.github.com>
2024-01-30 17:35:19 +0000

    Create SubArraySumK.java

---
+++ SubArraySumK.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
