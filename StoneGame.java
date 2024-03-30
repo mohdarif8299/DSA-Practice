9457afeff1c6594790c7333e89ad766105cc295b
Mohd Arif <31352655+mohdarif8299@users.noreply.github.com>
2024-03-31 04:30:26 +0530

    Create StoneGame.java

---
+++ StoneGame.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+         int n = piles.length;
+    
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+        
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        
+        return dp[0][n - 1] > 0;
+    }
+}
